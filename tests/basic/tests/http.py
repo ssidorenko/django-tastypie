925565aff83ea0864510e60ff5f9cd717461dbdf
Semion Sidorenko <semion.sidorenko@gmail.com>
2012-11-13 14:30:33 +0100

    Added a test case for the XSS in limit parameter

--- tests/basic/tests/http.py
+++ tests/basic/tests/http.py
@@ -39,6 +39,17 @@ class HTTPTestCase(TestServerTestCase):
         self.assertEqual(response.status, 200)
         self.assertEqual(response.read(), '{"meta": {"limit": 20, "next": null, "offset": 0, "previous": null, "total_count": 2}, "objects": [{"content": "This is my very first post using my shiny new API. Pretty sweet, huh?", "created": "2010-03-30T20:05:00", "id": 1, "is_active": true, "resource_uri": "/api/v1/notes/1/", "slug": "first-post", "title": "First Post!", "updated": "2010-03-30T20:05:00", "user": "/api/v1/users/1/"}, {"content": "The dog ate my cat today. He looks seriously uncomfortable.", "created": "2010-03-31T20:05:00", "id": 2, "is_active": true, "resource_uri": "/api/v1/notes/2/", "slug": "another-post", "title": "Another Post", "updated": "2010-03-31T20:05:00", "user": "/api/v1/users/1/"}]}')
 
+    def test_limit_xss(self):
+        """
+        Ensure that html cannot be injected in a wrong limit parameter
+        """
+        connection = self.get_connection()
+        connection.request('GET', '/api/v1/notes/?limit=<img%20src="http://ycombinator.com/images/y18.gif">', headers={'Accept': 'application/json'})
+        response = connection.getresponse()
+        connection.close()
+        self.assertEqual(response.status, 400)
+        self.assertTrue("<" not in response.read())
+
     def test_post_object(self):
         connection = self.get_connection()
         post_data = '{"content": "A new post.", "is_active": true, "title": "New Title", "slug": "new-title", "user": "/api/v1/users/1/"}'
