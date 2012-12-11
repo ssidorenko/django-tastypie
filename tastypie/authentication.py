58e626f800c35b681deb62c5be0b0987987553ae
Jeremy <jeremy.bluteau@synlab.com>
2012-12-11 15:56:16 +0100

    Add possibility to get username and api_key from META header.

--- tastypie/authentication.py
+++ tastypie/authentication.py
@@ -166,8 +166,8 @@ class ApiKeyAuthentication(Authentication):
 
             username, api_key = data.split(':', 1)
         else:
-            username = request.GET.get('username') or request.POST.get('username')
-            api_key = request.GET.get('api_key') or request.POST.get('api_key')
+            username = request.GET.get('username') or request.POST.get('username') or request.META.get('username')
+            api_key = request.GET.get('api_key') or request.POST.get('api_key') or request.META.get('api_key')
 
         return username, api_key
 
